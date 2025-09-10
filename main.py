303296b8ecede0d2c98b2fc304dfb0b209d389f0
Zhengyang Lu <stevenlu176@gmail.com>
2025-09-10 18:36:28 +0200

    Test parser on new domain definition

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from pddl_parser.pddl_parser import PddlProblemParser, parse_plan
 from dispatcher.dispatcher import CommandDispatcher
 from pathlib import Path
+
 from os import listdir
+from os.path import exists
+from os import listdir
+from os import makedirs
+
 
 import subprocess
 
@@ -21,28 +26,26 @@ def solve_pddl_problem(domain_name: str, problem_name: str, solver: str = "downw
             raise NotImplementedError()
 
 def main():
-    problem_name = "blocks_problem_3"
+    problem_name = "blocks_problem_5"
 
-    pp = PddlProblemParser("many_stacked", "blocks")
-    pp.define_problem(problem_name=problem_name, save=False)
+    pp = PddlProblemParser("stacked_test", "blocks")
+    pp.define_problem(problem_name=problem_name, save=True)
 
     solve_pddl_problem("blocks", problem_name)
 
     plans_dir = f"./plans/{problem_name}/"
 
-    try:
-        plan_files = listdir(plans_dir)
-    except:
-        subprocess.run(["mkdir", plans_dir])
-        plan_files = listdir(plans_dir)[-1]
+    if not exists(plans_dir):
+        makedirs(plans_dir)
 
+    plan_files = listdir(plans_dir)
     plan_file_nums = [int(f[-1]) for f in plan_files]
     plan_file = plan_files[plan_file_nums.index(max(plan_file_nums))]
     plan = parse_plan(plans_dir + plan_file)
 
-    cd = CommandDispatcher(pp.init_predicates, pp.positions)
-    cd.initialize_objects()
-    cd.run_simulation(plan)
+    # cd = CommandDispatcher(pp.init_predicates, pp.positions)
+    # cd.initialize_objects()
+    # cd.run_simulation(plan)
     # cd.run_simulation([("", [" ", " "])])
 
 if __name__ == "__main__":
