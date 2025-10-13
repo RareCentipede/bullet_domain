0479902136c3344fd39c9355d4968eca77abad45
Zhengyang Lu <stevenlu176@gmail.com>
2025-10-13 17:44:43 +0200

    Initialize D-LGP implementation

---
+++ d_lgp/dynamic_logic_geometric_programmer.py
@@ -0,0 +1,31 @@
+from abc import abstractmethod
+from typing import List, Tuple, Dict, Union, Callable
+
+from pypddl.core import States, State
+from pypddl.block_domain import at, not_at, gripper_empty, at_top, holding, clear, pose_supported, At
+from pypddl.block_domain import Object, Pose, Block, Robot, move, grasp, place
+
+def conflict_driven_task_graph(states: States, action_skeleton: List, goals: List) -> None:
+    current_state = states.states[-2]
+    goal_state = states.goal_states
+
+    action, args = successor_dagger(current_state, goal_state)
+    action_skeleton.append({'action': action, 'args': args})
+
+    conflicts = action(args)
+
+    while conflicts:
+        a_r, args, s = resolve_conflits(conflicts)
+        action_skeleton.insert(len(action_skeleton)-2, a_r)
+        goals.insert(len(goals)-2, s)
+
+        conflicts = action(args)
+
+def successor_dagger(current_state: State, goal_state: State) -> Tuple[Callable, Tuple[str, ...]]:
+    return (callable, ())
+
+def successor(current_state: State, action: str, action_args: Tuple[str, ...]) -> State:
+    return State({})
+
+def resolve_conflits(conflicts: List[Dict[str, Union[str, List[str]]]]) -> Tuple[Callable, Tuple[str, ...], List[State]]:
+    return (callable, (), [])
--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 from pypddl.block_domain import at, not_at, gripper_empty, at_top, holding, clear, pose_supported, At
-from pypddl.block_domain import Object, Pose, Block, Robot, States, move, grasp, place
+from pypddl.block_domain import Object, Pose, Block, Robot, move, grasp, place
+from pypddl.core import States, State
 from pddl_parser.problem_parser import parse_config_to_states
 
-predicate_dict = {'at': at,
-                  'not_at': not_at,
-                  'gripper_empty': gripper_empty,
-                  'at_top': at_top,
-                  'holding': holding,
-                  'clear': clear,
-                  'pose_supported': pose_supported}
+predicates = {'at': at,
+              'not_at': not_at,
+              'gripper_empty': gripper_empty,
+              'at_top': at_top,
+              'holding': holding,
+              'clear': clear,
+              'pose_supported': pose_supported}
 
 def test():
     states = parse_config_to_states('basic')
+    states.initialize_states()
     robot = states.get_obj_of_type('robot', Robot)
     block_2 = states.get_obj_of_type('block_2', Block)
-
-    print(at.evaluated_predicates)
 
     for p in states.init_states.values():
         print(all(list((p.values()))))
