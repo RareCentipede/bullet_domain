3c57fe6b5ba957c6b5a80dc53d5fd7c8c8f9aa02
Zhengyang Lu <Z.lu-5@student.tudelft.nl>
2025-10-16 13:14:58 +0200

    progress on DLGP and testing

--- d_lgp/dynamic_logic_geometric_programmer.py
+++ d_lgp/dynamic_logic_geometric_programmer.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod
-from typing import List, Tuple, Dict, Union, Callable
+from typing import List, Tuple, Dict, Union, Callable, Type
 
-from pypddl.core import States, State
+from pypddl.core import States, State, Thing
 from pypddl.block_domain import at, gripper_empty, at_top, holding, clear, pose_supported, At
 from pypddl.block_domain import Object, Pose, Block, Robot, move, grasp, place
 
@@ -13,41 +13,43 @@ def dynamic_tree_search(states: States) -> Tuple[List[Dict[str, Union[List[Calla
     while not states.goal_reached:
         last_feasible_action, last_feasible_action_args = action_skeleton[-1].items()
 
-        current_state = states.states[-2]
-        next_state = successor(current_state, last_feasible_action, last_feasible_action_args)
-        states.states.insert(len(states.states)-2, next_state)
+        action_results = last_feasible_action(states.current_state, *last_feasible_action_args)
+        next_state = action_results.new_state
+        states.update_states(next_state)
 
         conflict_driven_task_graph(states, action_skeleton, goals)
 
     return action_skeleton, goals
 
 def conflict_driven_task_graph(states: States, action_skeleton: List, goals: List) -> None:
-    current_state = states.states[-2]
+    current_state = states.current_state
     goal_state = states.goal_states
 
     action, args = successor_dagger(current_state, goal_state)
     action_skeleton.append({'action': action, 'args': args})
 
-    conflicts = action(args)
+    action_results = action(current_state, args)
+    conflicts = action_results.failed_preconditions
 
     while conflicts:
         a_r, args, s = resolve_conflicts(states, conflicts)
-        action_skeleton.insert(len(action_skeleton)-2, a_r)
-        goals.insert(len(goals)-2, s)
+        action_skeleton.insert(len(action_skeleton)-1, {'action': a_r, 'args': args})
+        goals.insert(len(goals)-1, s)
 
-        conflicts = action(args)
-
-def successor(current_state: State, action: Callable, action_args: Tuple[str, ...]) -> State:
-    """
-        Progress to the next state, the action is assumed to be feasible.
-    """
-
-    action(action_args)
-
-    return State({})
+        conflicts = action(s, args)
 
 def successor_dagger(current_state: State, goal_state: State) -> Tuple[Callable, Tuple[str, ...]]:
-    return (callable, ())
+    goal = ()
+
+    # Select a goal state
+    for args, true in goal_state['at'].items():
+        if not true:
+            args = list(args)
+            args.insert(0, 'at')
+            goal = tuple(args)
+            break
+
+    return place, goal
 
 def resolve_conflicts(states: States, conflicts: List[Dict[str, Union[str, List[str]]]]) -> Tuple[Callable, Tuple[str, ...], List[State]]:
     return (callable, (), [])
--- test.py
+++ test.py
@@ -2,23 +2,30 @@ from pypddl.block_domain import at, gripper_empty, at_top, holding, clear, pose_
 from pypddl.block_domain import Block, Robot, move, grasp, place
 from pddl_parser.problem_parser import parse_config_to_states
 
+from d_lgp.dynamic_logic_geometric_programmer import dynamic_tree_search, successor_dagger, resolve_conflicts
+
 def test():
     states = parse_config_to_states('basic')
     states.initialize_states()
     robot = states.get_obj_of_type('robot', Robot)
     block_2 = states.get_obj_of_type('block_2', Block)
+    print([p for p in states.poses])
+    a, args = successor_dagger(states.current_state, states.goal_states)
+    params = states.objects[args[1]]
+    pose = states.poses[args[2]]
+    print(a(states.current_state, robot=robot, object=params, target_pose=pose))
 
-    results = move(states.init_states, robot=robot, init_pose=robot.pose, target_pose=block_2.pose)
-    states.update_states(results.new_state)
+    # results = move(states.init_states, robot=robot, init_pose=robot.pose, target_pose=block_2.pose)
+    # states.update_states(results.new_state)
 
-    results = grasp(states.current_state, robot=robot, target_object=block_2, object_pose=block_2.pose)
-    states.update_states(results.new_state)
+    # results = grasp(states.current_state, robot=robot, target_object=block_2, object_pose=block_2.pose)
+    # states.update_states(results.new_state)
 
-    results = move(states.current_state, robot=robot, init_pose=robot.pose, target_pose=block_2.goal_pose)
-    states.update_states(results.new_state)
+    # results = move(states.current_state, robot=robot, init_pose=robot.pose, target_pose=block_2.goal_pose)
+    # states.update_states(results.new_state)
 
-    results = place(states.current_state, robot=robot, object=block_2, target_pose=block_2.goal_pose)
-    states.update_states(results.new_state)
+    # results = place(states.current_state, robot=robot, object=block_2, target_pose=block_2.goal_pose)
+    # states.update_states(results.new_state)
 
 if __name__ == "__main__":
     test()
